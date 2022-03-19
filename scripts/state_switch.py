47affe70fed46b4c0a2ff49848a467a506b6ea85
shubham-botsync <shubham.k@botsync.co>
2022-03-19 12:24:54 +0530

    added state_switch.py

---
+++ scripts/state_switch.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from time import sleep
+from std_msgs.msg import String
+import time
+import os
+import rospy
+import sys
+import random
+
+print(sys.argv)
+
+
+class StateSwitch:
+
+    def __init__(self):
+
+        self.state_list = ["NAVIGATION", "NAVFORWARD", "NAVRIGHT", "NAVLEFT",  "DOCK", "IDLE", "UNDOCK", "OBSTACLE", "WAIT_FOR_USER_INPUT",
+                           "CHARGING", "LOWBATTERY", "INITIALISING", "PAUSE",  "ERROR", "FAULT", "ESTOP", "STO", "TELEOP", "MAPPING", "AUTO", "NOSAFETY"]
+
+        self.state_pub = rospy.Publisher('robot_state', String, queue_size=1)
+
+    # def switch_states(time_duration):
+
+
+if __name__ == "__main__":
+
+    rospy.init_node('state_switch_node', anonymous=False)
+
+    state_switch_ = StateSwitch()
+
+    sz_ = len(state_switch_.state_list)
+
+    rate = rospy.Rate(0.2)
+
+    while not rospy.is_shutdown():
+
+        rand_idx_ = random.randint(0, sz_ - 1)
+
+        curr_state_ = state_switch_.state_list[rand_idx_]
+
+        state_switch_.state_pub.publish(curr_state_)
